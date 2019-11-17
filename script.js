0cedd3e74fee5c941a68da19724ba46205926e98
ermannod <erma-d@hotmail.com>
2019-11-17 21:23:11 +0100

    email list

---
+++ script.js
@@ -0,0 +1,14 @@
+// get email
+var email = prompt("Enter you e-mail address");
+console.log(email);
+// Registered emails
+var elist = [ "email0@email.com" , "email1@email.com" , "email2@email.com" , "email3@email.com" , "email4@email.com" , "email5@email.com" , "email6@email.com" , "email7@email.com" , "email8@email.com" ];
+console.log(elist);
+
+// Email checker
+if ( email = elist) {
+console.log("Your email is correct");
+}
+else {
+  console.log("Please verify your email");
+}
