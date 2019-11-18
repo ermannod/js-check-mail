6e9fdad51baed4ee1b26630e81d10793eb211f2c
ermannod <erma-d@hotmail.com>
2019-11-18 15:58:48 +0100

    emails

--- script.js
+++ script.js
@@ -5,10 +5,24 @@ console.log(email);
 var elist = [ "email0@email.com" , "email1@email.com" , "email2@email.com" , "email3@email.com" , "email4@email.com" , "email5@email.com" , "email6@email.com" , "email7@email.com" , "email8@email.com" ];
 console.log(elist);
 
-// Email checker
-if ( email = elist) {
-console.log("Your email is correct");
+var find = false;
+
+for (var i = 0; i < elist.length; i++) {
+  console.log(i);
+  var  currentEmail = elist[i];
+  console.log(currentEmail);
+  if ( currentEmail == email) {
+    console.log("Your email is correct");
+    find = true;
+  }
 }
-else {
-  console.log("Please verify your email");
+
+console.log("Is email in list? " + find);
+
+// Email checker
+
+if (find == true ){
+  console.log("your email is in");
+} else {
+  console.log("Please check your email");
 }
